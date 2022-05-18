cc019bf70c3b582d2b3623d453e9486b9ef03321
Shery Shajan <sheryjebu@gmail.com>
2022-05-18 01:31:34 +0100

    second commit

---
+++ Encapsulation_TestCase/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace Encapsulation_TestCase;
+
+public class WeatherReporterTests
+{
+    private string _location;
+    private double _temp;
+    
+    [SetUp]
+    public void Setup()
+    { 
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
